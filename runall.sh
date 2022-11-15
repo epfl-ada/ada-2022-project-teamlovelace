587144e0dfa506071de46c32c96bd08a298213a7
Nicole Vadot <ninivert1@gmail.com>
2022-11-15 19:51:02 +0100

    fixed merge conflicts and other fun stuff

--- runall.sh
+++ runall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 jupyter run src/preprocess.ipynb > /dev/null
-tree -I "__pycache__" > tree.txt
+tree -I "__pycache__|site_libs" > tree.txt
 conda env export -n ada2022teamlovelace > env.yaml
