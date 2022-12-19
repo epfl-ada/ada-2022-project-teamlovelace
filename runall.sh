0d420f1ee55305d67f2c42f9ae95f8734f4162a7
Nicole Vadot <ninivert1@gmail.com>
2022-12-19 13:08:06 +0100

    gitignore old files

--- runall.sh
+++ runall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 jupyter run src/preprocess.ipynb > /dev/null
 jupyter run src/preprocess_graph.ipynb > /dev/null
-tree -I "__pycache__|site_libs" > tree.txt
+tree -I "__pycache__|site_libs|*.old.*" > tree.txt
 conda env export -n ada2022teamlovelace > env.yaml
